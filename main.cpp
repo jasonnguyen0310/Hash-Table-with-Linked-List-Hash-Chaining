f08ebbdb2c3968be653da6f2c41f2fa92040ce80
jasonnguyen0310 <jasonnguyen0310@gmail.com>
2021-04-07 20:22:55 -0700

    First version of main.cpp finished

--- main.cpp
+++ main.cpp
@@ -6,7 +6,22 @@ Date: April 7 2021
 
 // Implement frequency counting
 
+#include "HashMap.hpp"
+#include <iostream>
+using namespace std;
+
 int main()
 {
+    HashMap h;
+    string word;
+    while(cin >> word)
+    {
+        h[word];
+    }
+
+    for (auto it = h.begin(); it != h.end(); ++it)
+    {
+        cout << it->first << "  -  " << it->second;
+    }
     return 0;
 }
